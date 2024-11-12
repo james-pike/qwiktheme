8395ea4c05fb8cda49f0e4da9b5f3d50c4eaae68
jvm3 <jamesandrewpike@gmail.com>
2024-11-12 18:54:24 -0500

    commit

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 
 // @ts-ignore
-import logoSrc from "~/assets/images/logo.png?width=64&height=64&png";
+import logoSrc from "~/assets/images/logo.svg";
 
 export default component$(() => (
   <span class="self-center ml-2 text-2xl md:text-xl font-bold text-gray-900 whitespace-nowrap dark:text-white flex items-center">
