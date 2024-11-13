255bb5bc900db651bbf1620671a53ebeddc275e0
James Pike <jamesandrewpike@gmail.com>
2024-11-13 13:23:01 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -11,8 +11,6 @@ export default component$(() => {
   <div class="absolute inset-0 -mt-28 overflow-hidden -z-10">
    <img width={600} height={600} src={HeroBackground} alt="Background cover image" class="object-cover w-full h-full" />
   </div>
-
-  
   <div class="relative max-w-7xl mx-auto px-4 sm:px-6 flex flex-col items-center text-center lg:flex-row lg:items-center lg:text-left">
     <div class="lg:basis-1/2 pb-10 md:pb-16">
       <h1 class="text-5xl md:text-6xl text-white sm:text-gray-900 font-bold leading-tighter tracking-tighter mb-4 font-heading">
@@ -23,7 +21,7 @@ export default component$(() => {
         Master the lines, embrace the quirks, and turn every twist into a masterpiece— because art's all about the journey, not the perfect line.
       </p>
       <div class=" sm:max-w-md flex flex-col sm:flex-row sm:justify-center gap-4 lg:justify-start">
-        <a class="btn bg-primary-500 w-full" href="https://aymansor.github.io/Etch-a-Sketch/" target="_blank" rel="noopener">Start Etching</a>
+        <a class="btn bg-primary-500 text-white font-bold w-full" href="https://aymansor.github.io/Etch-a-Sketch/" target="_blank" rel="noopener">Start Etching</a>
         <button class="btn w-full bg-gray-50">Learn More</button>
       </div>
     </div>
