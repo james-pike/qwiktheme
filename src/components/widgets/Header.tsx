d3a5ff4962ef3d9338f0ed4f71493a2e5d3a92cd
James Pike <jamesandrewpike@gmail.com>
2024-11-13 12:07:27 -0500

    commit

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -53,7 +53,7 @@ export default component$(() => {
                 <li key={key} class={items?.length ? "dropdown" : ""}>
                   {items?.length ? (
                     <>
-                      <button class="hover:text-link dark:hover:text-white px-4 py-3 flex items-center text-gray-900">
+                      <button class="hover:text-link dark:hover:text-white px-4 py-3 flex items-center text-gray-900 dark:text-white">
                         {text} <IconChevronDown class="w-3.5 h-3.5 ml-0.5 rtl:ml-0 rtl:mr-0.5 hidden md:inline" />
                       </button>
                       <ul class="dropdown-menu md:backdrop-blur-md dark:md:bg-slate-800 rounded md:absolute pl-4 md:pl-0 md:hidden font-medium md:bg-white/90 md:min-w-[200px] drop-shadow-xl">
