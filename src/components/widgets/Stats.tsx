5b8afc1313f18edff6d53fd0358cb795bbb4d507
James Pike <jamesandrewpike@gmail.com>
2024-11-13 09:51:49 -0500

    commit

--- src/components/widgets/Stats.tsx
+++ src/components/widgets/Stats.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div class="px-4 pt-8 pb-12 md:py-16 sm:px-6 mx-auto md:px-24 lg:px-8 lg:py-20 max-w-7xl">
+    <div class="px-4 pt-8 dark:bg-gray-900 pb-12 md:py-16 sm:px-6 mx-auto md:px-24 lg:px-8 lg:py-20 max-w-7xl">
       <div class="grid grid-cols-2 row-gap-8 md:grid-cols-4">
         <div class="text-center md:border-r dark:md:border-slate-500 mb-10 md:mb-0">
           <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-primary-500  font-heading">
@@ -33,7 +33,7 @@ export default component$(() => {
             N/A
           </div>
           <p class="text-sm font-medium tracking-widest text-gray-800 dark:text-slate-400 uppercase lg:text-base">
-            Designs worth keeping
+            Designs Finished
           </p>
         </div>
       </div>
