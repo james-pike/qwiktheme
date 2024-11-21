18b6614bd3163481144a4770c75b42fe788cd11b
James Pike <jamesandrewpike@gmail.com>
2024-11-21 17:03:10 -0500

    commit

---
+++ src/routes/(tabs)/tabs/index.tsx
@@ -0,0 +1,16 @@
+import { component$ } from "@builder.io/qwik";
+import SettingsTab from "~/components/widgets/SettingsTab";
+
+
+export default component$(() => {
+  
+
+ 
+
+  return (
+   <div>
+    <SettingsTab/>
+   </div>
+  );
+});
+
