66717caed50aae42bd2b16fcbb8a0d9ef4d47f53
James Pike <jamesandrewpike@gmail.com>
2024-11-21 14:50:52 -0500

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-        bgPrimary: "var(--color-bg-primary)",
-        tBase: "var(--color-text-base)",
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        bgPrimary: 'var(--color-bg-primary)',
+        tBase: 'var(--color-text-base)',
       },
       fontFamily: {
         sans: ["'Inter Variable'", ...defaultTheme.fontFamily.sans],
-      },
-      keyframes: {
-        translateX: {
-          "0%": { transform: "translateX(-100%)" },
-          "100%": { transform: "translateX(0%)" },
-        },
-        gradient: {
-          "0%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
-          "100%": { backgroundPosition: "0% 50%" },
-        },
-      },
-      animation: {
-        translateX: "translateX 10s infinite linear",
-        gradient: "gradient 10s ease infinite",
       },
     },
   },
