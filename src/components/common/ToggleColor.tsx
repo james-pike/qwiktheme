a7f66a74e4ed7d55e100dafbcdf5a54237efd75a
James Pike <jamesandrewpike@gmail.com>
2024-11-21 09:49:19 -0500

    commit

--- src/components/common/ToggleColor.tsx
+++ src/components/common/ToggleColor.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import { Popover } from "@qwik-ui/headless";
 import IconApps from "../icons/IconApps";
 import { useTheme } from "~/lib/provider";
@@ -31,32 +31,44 @@ export default component$((props: ItemProps) => {
     isDarkMode ? theme.mode === "dark" : theme.mode === "light"
   );
 
+  const isOpen = useSignal(false);
+
+
   return (
-    <Popover.Root flip={false} gutter={8}>
+    <Popover.Root>
       <Popover.Trigger>
-        <div class="px-4 py-2 font-medium text-white bg-primary rounded-md cursor-pointer">
+        <div
+          class="px-4 py-2 font-medium text-white bg-primary rounded-md cursor-pointer"
+          onClick$={() => (isOpen.value = !isOpen.value)} // Toggle popover state
+        >
           <IconApps class={iconClass} />
         </div>
       </Popover.Trigger>
-      <Popover.Panel>
-        <div class="grid gap-4 p-4 bg-gray-100 border border-gray-300 rounded-md shadow-md overflow-hidden">
-          <h4 class="font-medium text-lg">Select a Theme</h4>
-          <div class="grid grid-cols-3 gap-2">
-            {filteredThemes.map((theme) => (
-              <div
-                key={theme.name}
-                class={`w-16 h-16 rounded-md ${theme.color} ${
-                  currentTheme === theme.name
-                    ? "ring-2 ring-offset-2 ring-black"
-                    : ""
-                }`}
-                onClick$={() => (themeSig.value = theme.name)}
-                aria-label={`Select ${theme.name} theme`}
-              />
-            ))}
+      {isOpen.value && ( // Render Popover.Panel only when isOpen is true
+        <Popover.Panel>
+          <div class="grid gap-4 p-4 bg-gray-100 border border-gray-300 rounded-md shadow-md overflow-hidden">
+            <h4 class="font-medium text-lg">Select a Theme</h4>
+            <div class="grid grid-cols-3 gap-2">
+              {filteredThemes.map((theme) => (
+                <div
+                  key={theme.name}
+                  class={`w-16 h-16 rounded-md ${theme.color} ${
+                    currentTheme === theme.name
+                      ? "ring-2 ring-offset-2 ring-black"
+                      : ""
+                  }`}
+                  onClick$={() => {
+                    isOpen.value = false;
+                    themeSig.value = theme.name; // Set theme
+                     // Close popover
+                  }}
+                  aria-label={`Select ${theme.name} theme`}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      </Popover.Panel>
+        </Popover.Panel>
+      )}
     </Popover.Root>
   );
 });
