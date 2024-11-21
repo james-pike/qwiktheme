01aca974c684cd0c185a995f18d81dfbd5470c9f
jvm3 <jamesandrewpike@gmail.com>
2024-11-21 00:11:04 -0500

    commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,23 +1,18 @@
 import { component$, Slot } from "@builder.io/qwik";
-
 import Footer from "~/components/widgets/Footer";
 import Header from "~/components/widgets/Header";
-
 import { ThemeProvider } from "~/lib/provider";
-
 export default component$(() => {
-  
- 
 
   return (
     <>
-    <ThemeProvider
-		  defaultTheme="light-green" themes={['light-green', 'dark-green', 'light-blue', 'dark-blue']} attribute="class">
-      <Header />
-      <main>
-        <Slot />
-      </main>
-      <Footer />
+      <ThemeProvider
+        defaultTheme="light-green" themes={['light-green', 'dark-green', 'light-blue', 'dark-blue']} attribute="class">
+        <Header />
+        <main>
+          <Slot />
+        </main>
+        <Footer />
       </ThemeProvider>
     </>
   );
