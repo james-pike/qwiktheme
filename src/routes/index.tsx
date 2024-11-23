2a1d07e6b0d6a37e9bb9180d08dcd0d18991d060
James Pike <jamesandrewpike@gmail.com>
2024-11-23 09:29:16 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -71,7 +71,6 @@ export default component$(() => {
   ]}
 />
       <Steps />
-      {/* <svg xmlns="http://www.w3.org/2000/svg" class="mx-0" viewBox="0 0 1000 100" fill="#FF6464"><path d="M0 0v100l500-48 500 48V0H0z" opacity=".5"></path><path d="M0 0h1000v52H0z" opacity=".5"></path><path d="M0 0v4l500 48 500-48V0H0z" opacity=".5"></path><path d="M0 0v4l500 48 500-48V0H0z"></path></svg> */}
 
       <FAQs
   title="Frequently Asked Questions"
