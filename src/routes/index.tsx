901ba59d41b9d056dedf4aa91a473af7a45a0dc0
James Pike <jamesandrewpike@gmail.com>
2024-11-23 09:30:25 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -26,7 +26,7 @@ export default component$(() => {
   
   return (
     <>
-    <div class={`${themeSig.value}`}/>
+    <div class={themeSig.value}>
     <Hero />
       <Features
   highlight="Features"
@@ -71,6 +71,7 @@ export default component$(() => {
   ]}
 />
       <Steps />
+      {/* <svg xmlns="http://www.w3.org/2000/svg" class="mx-0" viewBox="0 0 1000 100" fill="#FF6464"><path d="M0 0v100l500-48 500 48V0H0z" opacity=".5"></path><path d="M0 0h1000v52H0z" opacity=".5"></path><path d="M0 0v4l500 48 500-48V0H0z" opacity=".5"></path><path d="M0 0v4l500 48 500-48V0H0z"></path></svg> */}
 
       <FAQs
   title="Frequently Asked Questions"
