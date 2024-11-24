0e6ce67b105e725cb0d6e29a45a36507ba230831
James Pike <jamesandrewpike@gmail.com>
2024-11-24 18:25:30 -0500

    commit

--- src/components/widgets/Pricing.tsx
+++ src/components/widgets/Pricing.tsx
@@ -9,12 +9,12 @@ export default component$(() => {
                 <div class="pb-5 md:py-0">
 
                     <div class="max-w-3xl mx-auto text-center p-6 rounded-md shadow-xl bg-white dark:bg-gray-900">
-                        <h2 class="text-4xl md:text-4xl font-bold leading-tighter tracking-tighter pt-2 my-4 font-heading">
+                        <h2 class="text-4xl md:text-4xl font-bold leading-tighter tracking-tighter pt-1 my-4 font-heading">
                             <span class="text-primary-500">Pricing Plans</span>
                             <br class="block sm:hidden" />
                         </h2>
                         <p class="text-xl text-gray-600 dark:text-slate-400">
-                            Get in touch to learn more about how the Etchy Sketchy can help level up your UX game. <br class="hidden md:inline" />
+                            Choose lump sum and self-host or pay as you go for continous hosting and updates. <br class="hidden md:inline" />
                         </p>
 
                         <div class="mt-6">
