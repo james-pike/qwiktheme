7d8c9e3fc74b87ecb0104b74b441b349d10d8716
James Pike <jamesandrewpike@gmail.com>
2024-11-25 17:26:55 -0500

    commit

--- src/components/widgets/PricingTab.tsx
+++ src/components/widgets/PricingTab.tsx
@@ -9,11 +9,11 @@ export default component$(() => {
         {/* Tab List */}
         <Tabs.List class="grid w-full grid-cols-2">
           <Tabs.Tab class="py-3 text-center bg-bgAccent border-b-2 border-transparent hover:border-primary text-secondary cursor-pointer">
-            <div class="text-sm font-medium">Custom Website</div>
+            <div class="text-sm font-medium">Full Website -</div>
             <div class="text-sm font-semibold">$5000</div>
           </Tabs.Tab>
           <Tabs.Tab class="relative py-3 text-center bg-bgAccent border-b-2 border-transparent hover:border-secondary text-primary cursor-pointer">
-            <div class="text-sm font-medium">Subscription</div>
+            <div class="text-sm font-medium">Subscription -</div>
             <div class="text-sm font-semibold">$250/month</div>
             {/* Ribbon for Popular Option */}
             <div class="absolute top-[-10px] right-[-10px] bg-secondary text-white text-xs px-2 py-1 rounded-full">
@@ -26,7 +26,7 @@ export default component$(() => {
         <Tabs.Panel class="-mt-0">
           <Card.Root class="bg-bgLight">
             <Card.Header>
-              <Card.Title class="text-primary">Complete Project</Card.Title>
+              <Card.Title class="text-primary">Complete Project - $5000 + $20/month hosting</Card.Title>
               <Card.Description>
                 A one-time payment covering all services for your custom website:
                 design, development, security, and initial setup.
@@ -52,7 +52,7 @@ export default component$(() => {
         <Tabs.Panel class="-mt-0">
         <Card.Root class="bg-bgLight">
             <Card.Header>
-              <Card.Title class="text-primary">Monthly Plan</Card.Title>
+              <Card.Title class="text-primary">Monthly Plan - $250/month</Card.Title>
               <Card.Description>
                 A recurring plan that ensures your website is up-to-date and secure:
                 hosting, maintenance, and ongoing support.
