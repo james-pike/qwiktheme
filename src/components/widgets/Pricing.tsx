a7c6344fa90a7a461412f147c7b29bf35ec5391a
James Pike <jamesandrewpike@gmail.com>
2024-11-25 14:54:11 -0500

    commit

--- src/components/widgets/Pricing.tsx
+++ src/components/widgets/Pricing.tsx
@@ -12,11 +12,11 @@ export default component$(() => {
                         
                         <div class="bg-bgAccent px-1 rounded-t-lg">
                         <h2 class="text-4xl md:text-4xl font-bold text-center leading-tighter tracking-tighter pt-8 my-0 font-heading">
-  <span class="text-primary-500">Flexible Pricing Options</span>
+  <span class="text-secondary">Flexible Pricing Options</span>
   <br class="block sm:hidden" />
 </h2>
 <p class="text-xl text-gray-600 text-center dark:text-slate-400 py-2 pb-6">
-  Choose a one-time code solution or subscribe to our monthly plan for continuous updates, hosting, and priority support.
+  Choose a one-time payment or subscribe to our monthly plan for continuous updates, hosting, and priority support.
 </p>
                         </div>
                         <div class="mt-0">
