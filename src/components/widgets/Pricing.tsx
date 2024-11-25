4ea2a337cdfbaa25e64b1e8a0ebb5ed7c9ca1c3f
James Pike <jamesandrewpike@gmail.com>
2024-11-25 09:15:40 -0500

    commit

--- src/components/widgets/Pricing.tsx
+++ src/components/widgets/Pricing.tsx
@@ -8,11 +8,11 @@ export default component$(() => {
             <div class="max-w-7xl mx-auto px-2 sm:px-6">
                 <div class="pb-2 md:py-0">
 
-                    <div class="max-w-3xl mx-auto p-0 rounded-md shadow-xl bg-white dark:bg-gray-900">
+                    <div class="max-w-3xl mx-auto p-0 rounded-md shadow-xl bg-bgAccent dark:bg-gray-900">
                         
                         <div class="bg-gradient-to-r from-bgAccent to-secondary px-1 rounded-t-lg">
                         <h2 class="text-4xl md:text-4xl font-bold text-center leading-tighter rounded-2xl tracking-tighter pt-4 my-2 font-heading">
-                            <span class="text-primary-500 mt-2 pt-4">Pricing</span>
+                            <span class="text-primary-500 pt-6">Pricing</span>
                             <br class="block sm:hidden" />
                         </h2>
                         <p class="text-xl text-gray-600 text-center dark:text-slate-400 pb-6">
