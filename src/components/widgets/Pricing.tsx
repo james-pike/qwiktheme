0b36dd145a3f2a25af88269109d15b81d1d52ef1
James Pike <jamesandrewpike@gmail.com>
2024-11-24 22:41:06 -0500

    commit

--- src/components/widgets/Pricing.tsx
+++ src/components/widgets/Pricing.tsx
@@ -4,19 +4,21 @@ import PricingTab from "./PricingTab";
 
 export default component$(() => {
     return (
-        <section class="relative bg-bgAccent py-2 sm:pb-20">
+        <section class="relative bg-bgAccent py-0 sm:pb-20">
             <div class="max-w-7xl mx-auto px-2 sm:px-6">
                 <div class="pb-2 md:py-0">
 
-                    <div class="max-w-3xl mx-auto p-3 rounded-md shadow-xl bg-white dark:bg-gray-900">
-                        <h2 class="text-4xl md:text-4xl font-bold text-center leading-tighter tracking-tighter pt-0 my-4 font-heading">
+                    <div class="max-w-3xl mx-auto p-0 rounded-md shadow-xl bg-white dark:bg-gray-900">
+                        
+                        <div class="bg-gradient-to-r from-bgAccent to-secondary px-1">
+                        <h2 class="text-4xl md:text-4xl font-bold text-center leading-tighter tracking-tighter pt-2 my-4 font-heading">
                             <span class="text-primary-500">Pricing</span>
                             <br class="block sm:hidden" />
                         </h2>
                         <p class="text-xl text-gray-600 text-center dark:text-slate-400">
                             Choose lump sum and self-host or pay as you go for continous hosting and updates. <br class="hidden md:inline" />
                         </p>
-
+                        </div>
                         <div class="mt-6">
                            
 
