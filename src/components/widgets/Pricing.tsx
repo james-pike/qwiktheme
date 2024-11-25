5ab1a82e83ad019b3a2fb19be26384cd9962a560
James Pike <jamesandrewpike@gmail.com>
2024-11-25 10:08:22 -0500

    commit

--- src/components/widgets/Pricing.tsx
+++ src/components/widgets/Pricing.tsx
@@ -11,11 +11,11 @@ export default component$(() => {
                     <div class="max-w-3xl mx-auto p-0 rounded-md shadow-xl bg-white dark:bg-gray-900">
                         
                         <div class="bg-bgAccent px-1 rounded-t-lg">
-                        <h2 class="text-4xl md:text-4xl font-bold text-center leading-tighter tracking-tighter py-8 my-0 font-heading">
+                        <h2 class="text-4xl md:text-4xl font-bold text-center leading-tighter tracking-tighter pt-8 my-0 font-heading">
                             <span class="text-primary-500 pt-0">Pricing</span>
                             <br class="block sm:hidden" />
                         </h2>
-                        <p class="text-xl text-gray-600 text-center dark:text-slate-400 pt-2">
+                        <p class="text-xl text-gray-600 text-center dark:text-slate-400 py-2">
                             Choose lump sum and self-host or pay as you go for continous hosting and updates. <br class="hidden md:inline" />
                         </p>
                         </div>
