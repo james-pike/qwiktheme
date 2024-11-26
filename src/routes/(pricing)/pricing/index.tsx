0c648066bb30961590ccd6c1b3f5161bdade598e
James Pike <jamesandrewpike@gmail.com>
2024-11-26 18:37:52 -0500

    commit

--- src/routes/(pricing)/pricing/index.tsx
+++ src/routes/(pricing)/pricing/index.tsx
@@ -1,5 +1,4 @@
 import { component$ } from "@builder.io/qwik";
-import Pricing from "~/components/widgets/Pricing";
 
 
 
@@ -12,7 +11,7 @@ export default component$(() => {
   return (
    <div>
     
-    <Pricing/>
+   
    </div>
   );
 });
