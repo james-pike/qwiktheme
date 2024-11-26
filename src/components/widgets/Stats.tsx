e8950082c06b6009994b78d2555a8056e31dae18
James Pike <jamesandrewpike@gmail.com>
2024-11-26 17:48:08 -0500

    commit

--- src/components/widgets/Stats.tsx
+++ src/components/widgets/Stats.tsx
@@ -5,7 +5,7 @@ export default component$(() => {
     <div class="px-4 pt-8 bg-gray-100 dark:bg-gray-900 pb-12 md:py-16 sm:px-6 mx-auto md:px-24 lg:px-8 lg:py-20 max-w-7xl">
       <div class="grid grid-cols-2 row-gap-8 md:grid-cols-4">
         <div class="text-center md:border-r dark:md:border-slate-500 mb-10 md:mb-0">
-          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-primary-500  font-heading">
+          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-primary-500 text-secondary font-heading">
             132K
           </div>
           <p class="text-sm font-medium tracking-widest text-gray-800 dark:text-slate-400 uppercase lg:text-base">
@@ -13,7 +13,7 @@ export default component$(() => {
           </p>
         </div>
         <div class="text-center md:border-r dark:md:border-slate-500 mb-10 md:mb-0">
-          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-primary-500 font-heading">
+          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-secondary font-heading">
             24.8K
           </div>
           <p class="text-sm font-medium tracking-widest text-gray-800 dark:text-slate-400 uppercase lg:text-base">
@@ -21,7 +21,7 @@ export default component$(() => {
           </p>
         </div>
         <div class="text-center md:border-r dark:md:border-slate-500 font-heading">
-          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-primary-500 ">
+          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-secondary ">
             10.3K
           </div>
           <p class="text-sm font-medium tracking-widest text-gray-800 dark:text-slate-400 uppercase lg:text-base">
@@ -29,7 +29,7 @@ export default component$(() => {
           </p>
         </div>
         <div class="text-center">
-          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-primary-500 font-heading">
+          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-secondary font-heading">
             N/A
           </div>
           <p class="text-sm font-medium tracking-widest text-gray-800 dark:text-slate-400 uppercase lg:text-base">
