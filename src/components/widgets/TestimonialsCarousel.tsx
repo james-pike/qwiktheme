0277542e9fa35277bf7d4c949669375018743bc4
James Pike <jamesandrewpike@gmail.com>
2024-11-26 17:00:40 -0500

    commit

--- src/components/widgets/TestimonialsCarousel.tsx
+++ src/components/widgets/TestimonialsCarousel.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik'
-import { Card, Carousel, Heading } from 'flowbite-qwik'
+import { Card, Carousel } from 'flowbite-qwik'
 
 export default component$(() => {
   return (
