39cc5b06a56a11573ad7395d7057084a5297de5f
James Pike <jamesandrewpike@gmail.com>
2024-11-26 19:55:30 -0500

    commit

--- src/routes/(pricing)/pricing/index.tsx
+++ src/routes/(pricing)/pricing/index.tsx
@@ -1,11 +1,6 @@
 import { component$ } from "@builder.io/qwik";
-import IconApps from "~/components/icons/IconApps";
-import IconBrandGoogle from "~/components/icons/IconBrandGoogle";
-import IconBrandTailwind from "~/components/icons/IconBrandTailwind";
-import IconBulb from "~/components/icons/IconBulb";
-import IconRocket from "~/components/icons/IconRocket";
+
 import Pricing from "~/components/widgets/Pricing";
-import PricingTab from "~/components/widgets/PricingTab";
 
 
 
