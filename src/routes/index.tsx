2885056b3b6cd52b62773ce180ebe5148f90c07c
James Pike <jamesandrewpike@gmail.com>
2024-11-27 15:05:47 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -158,7 +158,7 @@ export default component$(() => {
 />
  */}
 
-      <Stats />
+      {/* <Stats /> */}
       {/* <svg xmlns="http://www.w3.org/2000/svg" class="mx-0" viewBox="0 0 1000 100" fill="#FF6464"><path d="M0 0v100l500-48 500 48V0H0z" opacity=".5"></path><path d="M0 0h1000v52H0z" opacity=".5"></path><path d="M0 0v4l500 48 500-48V0H0z" opacity=".5"></path><path d="M0 0v4l500 48 500-48V0H0z"></path></svg> */}
      
       <Contact2  highlight="Contact Us"
