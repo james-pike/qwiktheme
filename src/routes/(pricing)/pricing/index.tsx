1b5a8409a08de60bc11eed3765309f2015af12b3
James Pike <jamesandrewpike@gmail.com>
2024-11-26 19:47:45 -0500

    commit

--- src/routes/(pricing)/pricing/index.tsx
+++ src/routes/(pricing)/pricing/index.tsx
@@ -1,4 +1,11 @@
 import { component$ } from "@builder.io/qwik";
+import IconApps from "~/components/icons/IconApps";
+import IconBrandGoogle from "~/components/icons/IconBrandGoogle";
+import IconBrandTailwind from "~/components/icons/IconBrandTailwind";
+import IconBulb from "~/components/icons/IconBulb";
+import IconRocket from "~/components/icons/IconRocket";
+import Pricing from "~/components/widgets/Pricing";
+import PricingTab from "~/components/widgets/PricingTab";
 
 
 
@@ -11,7 +18,14 @@ export default component$(() => {
   return (
    <div>
     
-   
+    <Pricing
+  highlight="Pricing"
+  title="Flexible Payment Options"
+  subtitle="Bringing you the latest in retro design tools—because digital brushes are overrated."
+  items={[  
+    {},
+  ] }
+/>
    </div>
   );
 });
