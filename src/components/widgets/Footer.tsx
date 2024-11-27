3fdbbe7731f54148bb6029ffbb011a4f77624b1e
James Pike <jamesandrewpike@gmail.com>
2024-11-27 15:33:03 -0500

    commit

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -49,7 +49,7 @@ export default component$(() => {
   ];
 
   return (
-    <footer class="py-2 bg-gradient-to-r from-secondary via-bgAccent to-secondary px-2">
+    <footer class="py-3 bg-gradient-to-r from-bgAccent via-secondary to-bgLight px-3">
       <div class="max-w-7xl mx-auto px-6 bg-bgLight dark:bg-gray-900 rounded-lg shadow-lg">
         <div class="grid grid-cols-12 gap-4 gap-y-8 sm:gap-8 pt-8 pb-2 md:py-12">
           <div class="col-span-12 lg:col-span-4 pr-8">
@@ -100,7 +100,7 @@ export default component$(() => {
           <div class="text-sm text-gray-700 mr-4 dark:text-slate-400 flex items-center">
             {/* Replace favicon span with your Logo component */}
             <Logo  />
-            Made with <span class="text-primary">♥</span> by{" "}
+            Made with <span class="text-primary">{" "}♥{" "}</span>by{" "}
             <a class="text-gray-900 underline dark:text-gray-200" href="#">
               Webdev.ca
             </a>{" "}
