370d6308276cf871395bf046edebeb8c4debe78c
James Pike <jamesandrewpike@gmail.com>
2024-11-28 00:30:25 -0500

    commit

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -1,10 +1,10 @@
 import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 
-import IconTwitter from "~/components/icons/IconTwitter";
-import IconInstagram from "~/components/icons/IconInstagram";
-import IconFacebook from "~/components/icons/IconFacebook";
-import IconGithub from "~/components/icons/IconGithub";
+// import IconTwitter from "~/components/icons/IconTwitter";
+// import IconInstagram from "~/components/icons/IconInstagram";
+// import IconFacebook from "~/components/icons/IconFacebook";
+// import IconGithub from "~/components/icons/IconGithub";
 import Logo from "../common/Logo";
 
 export default component$(() => {
@@ -25,28 +25,28 @@ export default component$(() => {
         { title: "Blog", href: "#" },
       ],
     },
-    {
-      title: "Support",
-      items: [
-        { title: "Contact Us", href: "#" },
-        { title: "FAQs", href: "#" },
-      ],
-    },
-    {
-      title: "Company",
-      items: [
-        { title: "About Us", href: "#" },
-        { title: "Privacy Policy", href: "#" },
-      ],
-    },
+    // {
+    //   title: "Support",
+    //   items: [
+    //     { title: "Contact Us", href: "#" },
+    //     { title: "FAQs", href: "#" },
+    //   ],
+    // },
+    // {
+    //   title: "Company",
+    //   items: [
+    //     { title: "About Us", href: "#" },
+    //     { title: "Privacy Policy", href: "#" },
+    //   ],
+    // },
   ];
 
-  const social = [
-    { label: "Twitter", icon: IconTwitter, href: "#" },
-    { label: "Instagram", icon: IconInstagram, href: "#" },
-    { label: "Facebook", icon: IconFacebook, href: "#" },
-    { label: "Github", icon: IconGithub, href: "#" },
-  ];
+  // const social = [
+  //   { label: "Twitter", icon: IconTwitter, href: "#" },
+  //   { label: "Instagram", icon: IconInstagram, href: "#" },
+  //   { label: "Facebook", icon: IconFacebook, href: "#" },
+  //   { label: "Github", icon: IconGithub, href: "#" },
+  // ];
 
   return (
     <footer class="py-3 bg-gradient-to-r from-bgAccent via-secondary to-bgLight px-3">
@@ -83,7 +83,7 @@ export default component$(() => {
           ))}
         </div>
         <div class="md:flex md:items-center md:justify-between py-6 md:py-8">
-          <ul class="flex mb-4 md:order-1 -ml-2 md:ml-4 md:mb-0">
+          {/* <ul class="flex mb-4 md:order-1 -ml-2 md:ml-4 md:mb-0">
             {social.map(({ label, href, icon: Icon }, index) => (
               <li key={index}>
                 <Link
@@ -96,7 +96,7 @@ export default component$(() => {
                 </Link>
               </li>
             ))}
-          </ul>
+          </ul> */}
           <div class="text-sm text-gray-700 mr-4 dark:text-slate-400 flex items-center">
             {/* Replace favicon span with your Logo component */}
             <Logo  />
--- src/components/widgets/grid2.tsx
+++ src/components/widgets/grid2.tsx
@@ -7,7 +7,6 @@ function getRandomInt(min: number, max: number): number {
 }
 
 export default component$(() => {
-  // Store for grid items with random animation properties
   const gridItemsStore = useStore({
     gridItems: new Array(30).fill(null).map(() => ({
       animationDelay: `${getRandomInt(0, 5)}s`,
@@ -15,15 +14,8 @@ export default component$(() => {
     })),
   });
 
-  // Task to apply the animation delay/duration after hydration
-  useTask$(() => {
-    // Logic to execute after component is hydrated, if needed
-    console.log('Grid items loaded and animations are ready.');
-  });
-
   return (
-    <div class="relative min-h-screen max-w-7xl mx-auto flex -mt-8 items-center justify-center bg-bgAccent overflow-hidden">
-      {/* Grid background */}
+    <div class="relative min-h-screen bg-bgAccent overflow-hidden">
       <div class="grid-background absolute inset-0 p-2 grid grid-cols-6 gap-1 transform -skew-y-0 scale-100">
         {gridItemsStore.gridItems.map((item, index) => (
           <div
@@ -37,98 +29,92 @@ export default component$(() => {
         ))}
       </div>
 
-      {/* Content */}
-     {/* Content */}
-     <div class="relative max-w-7xl mx-auto lg:px-12 lg:gap-20 lg:mt-8 px-4 sm:px-6 flex flex-col items-center text-center lg:flex-row lg:items-center lg:text-left">
-          <div class="lg:basis-1/2 pb-10 md:pb-16">
-           
-        <div
-          class="mt-12 rounded-lg border border-secondary bg-bgAccent p-8 lg:p-12"
-         
-        >
-          <div class="flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
-            <form action="#" method="POST" class="lg:flex-auto">
-              <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                <div>
-                  <label for="first-name" class="block text-sm/6 font-semibold text-gray-900">
-                    First name
-                  </label>
-                  <div class="mt-2.5">
-                    <input
-                      type="text"
-                      name="first-name"
-                      id="first-name"
-                      autocomplete="given-name"
-                      class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label for="last-name" class="block text-sm/6 font-semibold text-gray-900">
-                    Last name
-                  </label>
-                  <div class="mt-2.5">
-                    <input
-                      type="text"
-                      name="last-name"
-                      id="last-name"
-                      autocomplete="family-name"
-                      class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label for="budget" class="block text-sm/6 font-semibold text-gray-900">Budget</label>
-                  <div class="mt-2.5">
-                    <input
-                      id="budget"
-                      name="budget"
-                      type="text"
-                      class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label for="website" class="block text-sm/6 font-semibold text-gray-900">Website</label>
-                  <div class="mt-2.5">
-                    <input
-                      type="url"
-                      name="website"
-                      id="website"
-                      class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                    />
-                  </div>
-                </div>
-                <div class="sm:col-span-2">
-                  <label for="message" class="block text-sm/6 font-semibold text-gray-900">Message</label>
-                  <div class="mt-2.5">
-                    <textarea
-                      id="message"
-                      name="message"
-                      rows={4}
-                      class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                    ></textarea>
-                  </div>
-                </div>
+      <div class="relative max-w-4xl mx-auto px-6 lg:px-8">
+        <div class="mt-12 rounded-lg border border-secondary bg-bgAccent p-8 lg:p-12">
+          <form action="#" method="POST" class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label for="first-name" class="block text-sm font-semibold text-gray-900">
+                First name
+              </label>
+              <div class="mt-2.5">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autocomplete="given-name"
+                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                />
               </div>
-              <div class="mt-10">
-                <button
-                  type="submit"
-                  class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Let’s talk
-                </button>
+            </div>
+            <div>
+              <label for="last-name" class="block text-sm font-semibold text-gray-900">
+                Last name
+              </label>
+              <div class="mt-2.5">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autocomplete="family-name"
+                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                />
               </div>
-              <p class="mt-4 text-sm/6 text-gray-500">
-                By submitting this form, I agree to the
-                <a href="#" class="font-semibold text-indigo-600">privacy&nbsp;policy</a>.
-              </p>
-            </form>
-          
-          </div>
+            </div>
+
+            <div>
+              <label for="budget" class="block text-sm font-semibold text-gray-900">Budget</label>
+              <div class="mt-2.5">
+                <input
+                  type="text"
+                  name="budget"
+                  id="budget"
+                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label for="website" class="block text-sm font-semibold text-gray-900">Website</label>
+              <div class="mt-2.5">
+                <input
+                  type="url"
+                  name="website"
+                  id="website"
+                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label for="message" class="block text-sm font-semibold text-gray-900">Message</label>
+              <div class="mt-2.5">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                ></textarea>
+              </div>
+            </div>
+
+            <div class="mt-10 sm:col-span-2">
+              <button
+                type="submit"
+                class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Let’s talk
+              </button>
+            </div>
+            <p class="mt-4 text-sm text-gray-500 sm:col-span-2">
+              By submitting this form, I agree to the{' '}
+              <a href="#" class="font-semibold text-indigo-600">
+                privacy policy
+              </a>
+              .
+            </p>
+          </form>
         </div>
       </div>
-        </div>
     </div>
   );
 });
