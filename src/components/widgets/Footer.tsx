a31a7392e38be2d4f347f6a2c51e0098d04fe8af
James Pike <jamesandrewpike@gmail.com>
2024-11-28 17:45:36 -0500

    commit

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -51,7 +51,7 @@ export default component$(() => {
   return (
     <footer class="py-3 bg-gradient-to-r from-bgAccent via-secondary to-bgLight px-3">
       <div class="max-w-7xl mx-auto px-6 bg-bgAccent dark:bg-gray-900 rounded-lg shadow-lg">
-        <div class="grid grid-cols-12 gap-4 gap-y-6 sm:gap-8 pt-8 pb-2 md:py-12">
+        <div class="grid grid-cols-12 gap-4 gap-y-4 sm:gap-8 pt-8 pb-2 md:py-12">
           <div class="col-span-12 lg:col-span-4 pr-8">
             <div class="mb-4">
               <Link class="inline-block font-bold text-2xl" href={"/"}>
